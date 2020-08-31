c53af91cd6df17841939e061434afb6e24151734
Administrator <1660108596@qq.com>
2020-08-31 14:53:34 +0800

    4

---
+++ conftest.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+'''
+@Time       : 2020/8/31 14:40
+@Author     : Joy
+@FileName   : conftest.py
+'''
+
+
--- tools/getvcode.py
+++
@@ -1,7 +0,0 @@
-import tesserocr
-from PIL import Image
-
-image = Image.open('test.png')
-
-image.show()  # 可以打印出图片，供预览
-print(tesserocr.image_to_text(image))
